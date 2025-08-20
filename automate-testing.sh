7f5cb468c94097eea869d0d4447c850e852620cc
JosephMclean118 <josephjmclean392@gmail.com>
2025-08-20 20:16:27 +0100

    updated solution based on model answer

--- automate-testing.sh
+++ automate-testing.sh
@@ -3,13 +3,9 @@
 echo "AUTOMATED TESTING SCRIPT"
 
 # activate the virtual environment
-source venv/bin/activate
+. ./venv/bin/activate
 
 echo "VIRTUAL ENVIRONMENT ACTIVATED"
 
-
-# execute test suite
-pytest
-
-# check if pytest was successful
+# execute test script and check if pytest was successful
 pytest && exit 0 || exit 1
